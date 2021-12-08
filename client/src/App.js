56c89de81fe76fc02ac11f3244cd6616cda32095
kwonsean <gownn12@naver.com>
2021-12-09 00:15:45 +0900

    feat: 데이터를 받고 전달하는 기능 추가
    - searchList를 useState로 관리하며 SearchBar에서 검색 결과 데이터를
      가져오고 ShoppingList로 결과를 뿌려준다

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 import { Container } from 'reactstrap'
 import SearchBar from './components/SearchBar'
+import ShoppingList from './components/ShoppingList'
 
 function App() {
+  const [searchList, setSearchList] = useState([])
+  useEffect(() => {
+    console.log('this is in App', searchList)
+  }, [searchList])
   return (
     <Container style={{ marginTop: 40 }}>
-      <SearchBar />
+      <SearchBar setSearchList={setSearchList} />
+      <ShoppingList searchList={searchList} />
     </Container>
   )
 }
