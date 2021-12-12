266fdae0fbc9f307eefd3fdda33d4a9d80df0f85
kwonsean <gownn12@naver.com>
2021-12-12 18:27:32 +0900

    feat: NavBar 생성

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Nav, NavItem, NavLink } from 'reactstrap'
+
+export default function NavBar({ isSearchPage, setIsSearchPage }) {
+  const navClick = (e) => {
+    if (e.target.innerHTML === '구매 목록') {
+      setIsSearchPage(false)
+    } else {
+      setIsSearchPage(true)
+    }
+  }
+  return (
+    <div>
+      <Nav pills>
+        <NavItem>
+          <NavLink active={isSearchPage} onClick={navClick}>
+            상품 검색 & 구매
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink active={!isSearchPage} onClick={navClick}>
+            구매 목록
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </div>
+  )
+}
